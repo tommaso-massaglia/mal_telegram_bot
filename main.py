4c2de97bcd7e66f3d49eb675c963cdff5081919e
tommaso-massaglia <tmassagl@gmail.com>
2023-11-17 11:38:32 +0100

    initial_commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# What should this bot do?
+# - Create a list of followed animes, different for each user
+# - Notify the user when an animme they added to the list receives a new episode
+# - Allow the user to search for animes (use telegram function to send "embeds")
+# - Allow user to remove followed animes from a list (maybe using the embeds)
+# - Allow the user to see what animes he added to the list and when will the next episode for each one be
+
+
